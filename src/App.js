f88e5a9befa6640bc1b094e76dd5972c6300587c
Elahe Jafari <e.jafari@tehrangroup.local>
2022-08-20 18:02:59 +0430

    Initial Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [Data, setData] = React.useState([{ id: 1, title: "saberi", status: "", Discription: "Bad Akhlagh" }]);
+
+    function addTodo(event) {
+        let newObject = {
+            id: uuidv4(),
+            title: event.target[0].value,
+            status: "",
+            Discription: ""
+        }
+
+        setData([...Data, newObject])
+        event.preventDefault()
+    }
+    function deleteTodo(item) {
+        let newArr = Data.filter((i) => {
+            return i.id != item.id
+        })
+        setData(newArr)
+    }
+    function edditeTodo(item) {
+        let eddite = window.prompt("")
+        let newItem = { ...item, title: eddite }
+        let newArr = Data.map((i) => {
+            if (i.id === newItem.id) {
+                return newItem
+            }
+            return i
+        })
+
+        setData(newArr)
+
+
+    }
+    return (
+        <div className="main">
+            <form className="header" onSubmit={addTodo}>
+                <input className="title" />
+                <input type="submit" value="Submit" />
+            </form>
+            <div className="continer">
+                {Data.map((props, index) => {
+                    return <div className="todo-item" key={index}>
+                        <h3>{props.title}</h3>
+                        <div>
+                            <button>Confirm</button>
+                            <button>Cancel</button>
+                        </div>
+                        <button onClick={edditeTodo.bind(null, props)}>Edit</button>
+                        <button onClick={deleteTodo.bind(null, props)}>Delete</button>
+                    </div>
+                })}
+
+            </div>
+        </div>
+    )
 }
-
-export default App;
