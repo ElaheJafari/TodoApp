f1a02052119f407c33c11d537414aafcdaf21275
ElaheJafari <elahejafari813@gmail.com>
2022-08-20 23:29:28 +0430

    create Todo Component, use Localstorage, Confirm & Cancle, Description

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import Todo from "./Todo";
 
 export default function App() {
-    const [Data, setData] = React.useState([{ id: 1, title: "saberi", status: "", Discription: "Bad Akhlagh" }]);
+    const [Data, setData] = React.useState([]);
+
+
+    useEffect(() => {
+        let Result = localStorage.getItem("MyData");
+        if (Result)
+            setData(JSON.parse(Result))
+    }, [])
+
+
+    useEffect(() => {
+        if (Data.length !== 0) {
+            localStorage.setItem("MyData", JSON.stringify(Data))
+        }
+    }, [Data])
 
     function addTodo(event) {
         let newObject = {
             id: uuidv4(),
             title: event.target[0].value,
             status: "",
-            Discription: ""
+            Discription: event.target[1].value,
         }
-
         setData([...Data, newObject])
         event.preventDefault()
     }
     function deleteTodo(item) {
         let newArr = Data.filter((i) => {
-            return i.id != item.id
+            return i.id !== item.id
         })
         setData(newArr)
     }
@@ -30,28 +44,27 @@ export default function App() {
             }
             return i
         })
-
         setData(newArr)
-
-
+    }
+    function Togle(item) {
+        let newArr = Data.map((i) => {
+            if (i.id === item.id) {
+                return item
+            }
+            return i
+        })
+        setData(newArr)
     }
     return (
         <div className="main">
             <form className="header" onSubmit={addTodo}>
                 <input className="title" />
+                <input className="des" />
                 <input type="submit" value="Submit" />
             </form>
             <div className="continer">
                 {Data.map((props, index) => {
-                    return <div className="todo-item" key={index}>
-                        <h3>{props.title}</h3>
-                        <div>
-                            <button>Confirm</button>
-                            <button>Cancel</button>
-                        </div>
-                        <button onClick={edditeTodo.bind(null, props)}>Edit</button>
-                        <button onClick={deleteTodo.bind(null, props)}>Delete</button>
-                    </div>
+                    return <Todo key={index} {...props} Togle={Togle} edditeTodo={edditeTodo} deleteTodo={deleteTodo} />
                 })}
 
             </div>
